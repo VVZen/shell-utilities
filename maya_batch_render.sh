e37f098d2b984a50e7648a2795da2a24884eea52
VVZen <VVZen@users.noreply.github.com>
2015-08-30 19:28:41 +0200

    Create maya_batch_render.sh

---
+++ maya_batch_render.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/Applications/Autodesk/maya2014/Maya.app/Contents/bin/Render -r file "/yourPath/yourfilename_1.mb"
+/Applications/Autodesk/maya2014/Maya.app/Contents/bin/Render -r file "/yourPath/yourfilename_2.mb"
